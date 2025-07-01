9610e23b37f5a6275a3db3f0951e1fee26d4e175
Abdul Rehman <abdulrehman@example.com>
2025-07-01 18:30:52 +0500

    complete listing page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
 
         <Link to='/'>
    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Rehman</span>
+            <span className='text-slate-500'>AR-</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
           </Link>
